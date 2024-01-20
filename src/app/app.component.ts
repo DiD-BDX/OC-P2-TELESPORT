61ac6697f3659def789b3e4908a33cc423e0d8e6
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-01-20 15:42:18 +0100

    Add new files and update existing files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { OlympicService } from './core/services/olympic.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'OCP2_TELESPORT';
+export class AppComponent implements OnInit {
+  constructor(private olympicService: OlympicService) {}
+
+  ngOnInit(): void {
+    this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+  }
 }
