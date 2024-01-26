f9b1fa3b2b4dfe9cde823ea5a42d6a19c6e7b02f
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-01-26 13:20:30 +0100

    Refactor HomeComponent to use takeUntil operator for unsubscribing from observables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
 import { OlympicCountry, detailsData } from '../../core/models/Olympic';
 import { OlympicService } from '../../core/services/olympic.service';
 import { Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
   public olympics$: Observable<OlympicCountry[]> = of([]);
   PieChartResults: detailsData[] = [];
   countryNameData : OlympicCountry[] = [];
@@ -21,22 +23,31 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/details', data.name]);
   }
   ngOnInit(): void {
-    this.olympicService.getCountryNameData().subscribe(data => {
-      this.countryNameData = data;
-      this.totalUniqueJOs = this.olympicService.getTotalUniqueJOs(data);
-    });
+    this.olympicService.getCountryNameData()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(data => {
+        this.countryNameData = data;
+        this.totalUniqueJOs = this.olympicService.getTotalUniqueJOs(data);
+      });
 
     this.olympics$ = this.olympicService.getOlympics();
-    this.olympics$.subscribe(olympicCountries => {
-      this.PieChartResults = olympicCountries.map(p => {
-        let totalParticipations = p.participations.length;
-        return {
-          label: p.country,
-          name : p.country,
-          value: totalParticipations,
-        };
+    this.olympics$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(olympicCountries => {
+        this.PieChartResults = olympicCountries.map(p => {
+          let totalParticipations = p.participations.length;
+          return {
+            label: p.country,
+            name : p.country,
+            value: totalParticipations,
+          };
+        });
       });
-    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
